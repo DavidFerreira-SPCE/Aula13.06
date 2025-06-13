b73ee775239ae393bdfc3125f36412e2d5ae9a52
David Ferreira <davidleandro670@gmail.com>
2025-06-13 11:38:21 -0300

    Realizado o Ex 11, mas terminar os 9 restantes

--- exercicio11.js
+++ exercicio11.js
@@ -1,5 +1,12 @@
 // 11 Crie uma função que receba um texto e retorne quantos caracteres esse texto possui. //
 
-function Texto(a = 1) {
-    const palavra =
+function Texto(Nome) {
+    // Declaro que exemplo recebe o que for digitado no console
+    let exemplo = Nome
+    // O tamanho receberá os digitos e contará todos os imputs realizados pelo usuário, incluindo espaços
+    let tamanho = exemplo.length;
+    // após contar, o tamanho receberá o número pela contagem length
+    return tamanho;
 }
+// O número será mostrado na tela
+console.log( "O Texto com espaços possui " + (Texto("David Ferreira Leandro Lima") + " caracteres"));
