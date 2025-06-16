c0b42f88902fdaf83e080e8683ac0286eb2fdbc9
David Ferreira <davidleandro670@gmail.com>
2025-06-16 09:39:21 -0300

    Tarefa finalizada

--- exercicio18.js
+++ exercicio18.js
@@ -1,5 +1,12 @@
 // 18 Crie uma função que receba um valor e um percentual e retorne o valor com o percentual aplicado (ex: desconto). //
 
 function desconto(valor) {
-    
+    let desconto = 0.1
+    let valorinicial = valor
+    let valor_final
+
+     valor_final = valor - (valor * desconto)
+     return valor_final
 }
+
+console.log("Seu valor total da compra após a aplicação do benefício (desconto) de 10% aplicado ficaram no valor de: R$ " + desconto(382.50))
