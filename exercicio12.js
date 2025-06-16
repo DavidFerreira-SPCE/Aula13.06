d861f5b77891e77a38b8d65650398d7a654a4769
David Ferreira <davidleandro670@gmail.com>
2025-06-16 08:20:46 -0300

    Feito Ex.12

--- exercicio12.js
+++ exercicio12.js
@@ -1,1 +1,9 @@
 // 12 Crie uma função que receba um texto e retorne o mesmo texto em letras maiúsculas.
+
+function Texto(Nome) {
+      let exemplo = Nome
+      let maiúsculas = exemplo.toUpperCase()
+      //É uma função do JavaScript para colocar caixa alta
+      return maiúsculas;
+}
+console.log("Mostrando a palavra em caixa alta: " + Texto("brilho eterno de uma mente sem lembranças"))
